617032542f77d30636639ebc69e7f0405d5dc3dd
debujanai <kashifjalal01@gmail.com>
2025-06-19 18:30:25 +0500

    phishtrace

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -62,6 +62,15 @@ const Sidebar = ({ isCollapsed, toggleSidebar }: SidebarProps) => {
       )
     },
     { 
+      name: 'PhishTrace', 
+      href: '/phishtrace', 
+      icon: (
+        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="text-red-400">
+          <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
+        </svg>
+      )
+    },
+    { 
       name: 'Reports', 
       href: '/reports', 
       icon: (
